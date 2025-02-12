201ab22eced9e158d5f7892617d31b6275412f8c
Brerras <nikos@drosinis.gr>
2025-02-12 10:46:41 +0200

    Update PostgreSQL_LocalAccount_Discovery.ps1
        Add SSL Mode and trust Certificate (if you have only IP) on connection string to connect Postgresql on Azure  (SaaS) 
        Add System.Threading.Tasks.Extensions.dll 
        Added Log file "C:\scripts\postgres.log"

--- Scripts/SecretServer/PostgreSQL/Discovery/PostgreSQL_LocalAccount_Discovery.ps1
+++ Scripts/SecretServer/PostgreSQL/Discovery/PostgreSQL_LocalAccount_Discovery.ps1
@@ -8,6 +8,12 @@
     NOTES
     The following logPath variable is used for troubleshooting when necessary; a file is written to this path with errors.
     A file will be created for each server and overwritten on each run.
+
+    Nick Drosinis
+    Add SSL Mode and trust Certificate (if you have only IP) on connection string to connect Postgresql on Azure  (SaaS) 
+    Add System.Threading.Tasks.Extensions.dll 
+    Added Log file "C:\scripts\postgres.log"
+
 #>
 
 $server = $args[0]
@@ -16,29 +22,48 @@ $database = 'postgres'
 $username = $args[1]
 $password = $args[2]
 
+# Log file path
+$logFile = "C:\scripts\postgres.log"
+
+# Function to write to the log file
+function Write-Log {
+    param (
+        [string]$message
+    )
+    $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+    $logMessage = "$timestamp - $message"
+    Add-Content -Path $logFile -Value $logMessage
+}
+
 $FoundPostgreSQLUsers = @()
 
 try {
     # Specify the path of the Npgsql .NET data provider
     $npgsqlDllPath = "C:\Program Files (x86)\PostgreSQL\Npgsql\bin\net451\Npgsql.dll"
+    $tasksDllPath = "C:\Program Files (x86)\PostgreSQL\Npgsql\bin\net451\System.Threading.Tasks.Extensions.dll"
 
     # Load the DLL file
-    [System.Reflection.Assembly]::LoadFile($npgsqlDllPath) | Out-Null  # Suppress output
-
-    # Construct the PostgreSQL connection string
-    $connectionString = "Host=$server;Port=$port;Database=$database;Username=$username;Password=$password;"
+    [System.Reflection.Assembly]::LoadFile($npgsqlDllPath) | Out-Null
+    [System.Reflection.Assembly]::LoadFile($tasksDllPath) | Out-Null
     
+    # Construct the PostgreSQL connection string
+																											
+    $connectionString = "Host=$server;Port=$port;Database=$database;User Id=$username;Password=$password;Ssl Mode=Require;Trust Server Certificate=True;"
+  
+    # Write the server connection to the log file  
+    #Write-Log "Connection : $server"
+
     # Create a connection to the PostgreSQL database
     $connection = New-Object Npgsql.NpgsqlConnection
     $connection.ConnectionString = $connectionString
     $connection.Open()
 
-    # Sorgu oluştur
+    # SQL Query to find admin accounts
     $query = "SELECT usename FROM pg_catalog.pg_user WHERE usename NOT IN ('postgres');"
     $command = $connection.CreateCommand()
     $command.CommandText = $query
 
-    # Sorguyu çalıştır ve sonuçları oku
+    # for IBM SecretServer
     $reader = $command.ExecuteReader()
 
     while ($reader.Read()) {
@@ -51,6 +76,7 @@ try {
     $reader.Close()
     $connection.Close()
 } catch {
+    Write-Log "Error: $($_.Exception.Message)"
     throw "Hata: $($_.Exception.Message)"
 }
 
